c3bb2348e9cc66e4f40ac83fe38f4c9b4bc41a02
kao126 <76607249+kao126@users.noreply.github.com>
2025-05-04 18:26:49 +0900

    update: use auth.config.ts file for auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
+import { authConfig } from '@/auth.config';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-});
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
