577fe433e92d0835330c804f065da0a7d4c6569f
kao126 <76607249+kao126@users.noreply.github.com>
2025-05-04 19:14:29 +0900

    feat: sign-in with google func

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,37 @@
-export default function Login() {
+'use clients';
+import { auth, signIn, signOut } from '@/auth';
+
+export default async function Login() {
+  const session = await auth();
+
+  // console.log(session?.idToken);
   return (
     <div>
       <h1>Login</h1>
       <p>ログイン画面</p>
+      {!session && (
+        <form
+          action={async () => {
+            'use server';
+            await signIn('google');
+          }}
+        >
+          <button type='submit'>Signin with Google</button>
+        </form>
+      )}
+      {session && (
+        <div>
+          <p>success!!! welcome kyle's photos</p>
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <button type='submit'>SignOut</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,8 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
+import Google from 'next-auth/providers/google';
 
 export const authConfig = {
-  providers: [],
+  providers: [Google],
   pages: {
     signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user && account?.id_token) {
+        token.idToken = account?.id_token;
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      session.idToken = token.idToken;
+      return session;
+    },
   },
 } satisfies NextAuthConfig;
