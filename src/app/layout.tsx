81474a43fbae852c8999f989c6df3a8724c38504
kao126 <76607249+kao126@users.noreply.github.com>
2025-05-31 09:37:19 +0900

    update: display header on mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/app-sidebar';
 import { Toaster } from 'sonner';
 
@@ -32,7 +32,12 @@ export default function RootLayout({
         <SessionProvider>
           <SidebarProvider>
             <AppSidebar />
-            <div>{children}</div>
+            <SidebarInset>
+              <header className='md:hidden flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 bg-background z-50'>
+                <SidebarTrigger className='-ml-1' />
+              </header>
+              <div>{children}</div>
+            </SidebarInset>
             <Toaster />
           </SidebarProvider>
         </SessionProvider>
