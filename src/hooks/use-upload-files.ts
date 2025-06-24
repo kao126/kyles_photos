d2e06c84e9abc6c18c4b7c0941d0116db77549bc
kao126 <76607249+kao126@users.noreply.github.com>
2025-06-24 21:13:55 +0900

    update: upload files in parallel

--- src/hooks/use-upload-files.ts
+++ src/hooks/use-upload-files.ts
@@ -13,24 +13,33 @@ export function useFileUploadLogic() {
       setUploading(true);
       const files = e.target.files;
       if (!files || files.length === 0) return;
+      const filesLength = files.length;
 
       const uploadingToastId = toast('アップロード中', {
-        description: `Uploading ${files.length} files`,
+        description: `Uploading ${filesLength} files`,
         duration: Infinity,
       });
 
+      const queue = [...files];
+      const concurrency = 3;
       let successCount = 0;
-      for (const file of files) {
+      async function uploadNext() {
+        const file = queue.shift();
+        if (!file) return;
+
         try {
           await uploadFiles({ file, userId }); // S3にアップロード
           successCount++;
-          setProgress(Math.round((successCount / files.length) * 100));
-          console.log('File uploaded successfully');
-        } catch (error) {
-          console.error('Error uploading file:', error);
-          throw error;
+          setProgress(Math.round((successCount / filesLength) * 100));
+        } catch (err) {
+          console.error(`Upload failed for ${file.name}:`, err);
         }
+
+        await uploadNext(); // 次のファイルに進む
       }
+
+      // 並列処理
+      await Promise.all(Array.from({ length: concurrency }, () => uploadNext()));
 
       setUploaded((prev) => !prev);
       toast.dismiss(uploadingToastId);
