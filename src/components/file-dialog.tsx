cfd2f0a8e9861a1b65abfdfb9291375de535e290
kao126 <76607249+kao126@users.noreply.github.com>
2025-07-01 21:57:20 +0900

    refactor: generate video thumbnail func

--- src/components/file-dialog.tsx
+++ src/components/file-dialog.tsx
@@ -1,7 +1,8 @@
 import { DialogContent, DialogTitle } from '@radix-ui/react-dialog';
 import { Dialog, DialogHeader } from '@/components/ui/dialog';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 import { FileDropDown } from './file-dropdown';
+import { useGenerateThumbnailLogic } from '@/hooks/use-generate-thumbnail';
 
 export function FileDialogContent({
   open,
@@ -14,6 +15,14 @@ export function FileDialogContent({
   selectedFile: SelectedFileType;
   isDeleted: MediaEntryType['isDeleted'];
 }) {
+  const { thumbnailUrl, setThumbnailUrl, generateThumbnail } = useGenerateThumbnailLogic();
+
+  useEffect(() => {
+    if (selectedFile.file.videoUrl) {
+      generateThumbnail(selectedFile.file.videoUrl).then(setThumbnailUrl);
+    }
+  }, [selectedFile.file.videoUrl]);
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className='absolute inset-0 bg-black/90 h-full flex flex-col'>
@@ -25,7 +34,7 @@ export function FileDialogContent({
         </DialogHeader>
         <div className='flex-1 flex justify-center items-center overflow-hidden pb-6 md:pb-12'>
           {selectedFile.file.videoUrl ? (
-            <video src={selectedFile.file.videoUrl} poster={selectedFile.file.imageUrl} controls className='w-auto h-full object-contain' />
+            <video src={selectedFile.file.videoUrl} poster={thumbnailUrl} controls className='w-auto h-full object-contain' />
           ) : (
             <img src={selectedFile.file.imageUrl} alt={selectedFile.file.fileName} className='w-auto h-full object-contain' />
           )}
--- src/components/video-thumbnail.tsx
+++ src/components/video-thumbnail.tsx
@@ -1,44 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useGenerateThumbnailLogic } from '@/hooks/use-generate-thumbnail';
+import { useEffect } from 'react';
 
 export function VideoThumbnail({ file }: { file: MediaEntryType }) {
-  const [thumbnailUrl, setThumbnailUrl] = useState<string>();
-
-  async function generateThumbnail(videoUrl: MediaEntryType['videoUrl']): Promise<string> {
-    if (!videoUrl) {
-      return '';
-    }
-    return new Promise((resolve, reject) => {
-      const video = document.createElement('video');
-      video.src = videoUrl;
-      video.crossOrigin = 'anonymous';
-      video.muted = true;
-      video.playsInline = true;
-
-      video.addEventListener(
-        'loadedmetadata',
-        () => {
-          video.play().then(() => {
-            video.currentTime = 0;
-          });
-        },
-        { once: true }
-      );
-
-      video.addEventListener('seeked', () => {
-        const canvas = document.createElement('canvas');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        const ctx = canvas.getContext('2d');
-        ctx?.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        const dataUrl = canvas.toDataURL('image/jpeg');
-        resolve(dataUrl);
-      });
-
-      video.addEventListener('error', () => reject('動画の読み込みに失敗'));
-    });
-  }
+  const { thumbnailUrl, setThumbnailUrl, generateThumbnail } = useGenerateThumbnailLogic();
 
   useEffect(() => {
     if (file.videoUrl) {
